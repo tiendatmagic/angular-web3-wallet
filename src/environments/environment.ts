afbe50ad03da8b83afef850aac4ef418a72a890b
tiendatmagic <tiendatmagic8@gmail.com>
2026-07-18 23:05:33 +0700

    feat: initialize project structure with environment configs, splash screen, and home UI components

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   walletConnectProjectId: 'a196657383cc397e36c797a54165e326', // Project ID mặc định của Angular Web3 Wallet
   defaultRpcUrl: 'https://arb1.arbitrum.io/rpc',
   defaultExplorerUrl: 'https://arbiscan.io',
-  defaultChainId: '42161', // Arbitrum One Mainnet
+  defaultChainId: '42161',
   apiBaseUrl: 'https://cafeweb3.ddns.net/api'
 };
